250a50084c74e408da569b4c3acb090826fb185a
scottbezzman-dot <scottbezzman@gmail.com>
2026-05-15 10:26:24 +0100

    fix cors error

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const session = require('express-session');
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://backedbyquantum.com'
+  'https://backedfrontend.vercl.app'
 ];
 
 app.use(cors({
